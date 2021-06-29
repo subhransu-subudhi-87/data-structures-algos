5f7dcce11a2a8905464c776903f9f230ee0ab6d1
subhransu-subudhi-87 <subhransu-subudhi87@gmail.com>
2021-06-29 23:47:06 +0530

    DynamicArray Implementation pushed. .gitignore file updated

---
+++ data-structures-algos/src/arrays/dynamic/DynamicArray.java
@@ -0,0 +1,58 @@
+package arrays.dynamic;
+
+public class DynamicArray<T> {
+
+	private int count;
+	private int[] dynamicArray;
+
+	public DynamicArray(int size) {
+		this.dynamicArray = new int[size];
+	}
+	
+	public void printDynamicArray() {
+		for(int i = 0; i < count ; i++) {
+			System.out.println(dynamicArray[i]);
+		}
+	}
+
+	public int dynamicArraySize() {
+//		 return dynamicArray.length; // Returns actual length
+		return count; // Returns number of elements present
+	}
+	
+	public void insert(int item) {
+		// 1. Check if array is full , then resize the array
+		if (count == dynamicArray.length) {
+			// a: Create a new Array(Twice the size of the old array)
+			int[] newDynamicArray = new int[count * 2];
+			// b: Copy the old array elements to the new array
+			for (int i = 0; i < count; i++)
+				newDynamicArray[i] = dynamicArray[i];
+			// c: Set the newArray ref to the old one
+			dynamicArray = newDynamicArray;
+		}
+		// 2. insert the new item at the end of the new array
+		dynamicArray[count++] = item;
+	}
+	
+	public void removeAt(int index) {
+		//1.Validate the index
+		if(index < 0 || index >= count)	
+			throw new IllegalArgumentException();
+		//2. Shift the items to their left side after removal of the item in the particular index
+		for(int i = index ; i < count ; i++)
+			dynamicArray[i] = dynamicArray[i+1];
+		//3.Decreament the size of the array
+		count--;
+	}
+	
+	public int searchItem(int item) {
+		//1. If item is found return the index
+		for(int i = 0 ; i < count ; i++)
+			if(dynamicArray[i] == item)
+				return i;
+		//2.Else return -1
+		return -1;
+	}
+	
+}//End of the Class
---
+++ data-structures-algos/src/arrays/dynamic/DynamicArrayDemo.java
@@ -0,0 +1,24 @@
+package arrays.dynamic;
+
+import java.util.Arrays;
+
+public class DynamicArrayDemo {
+
+	
+	public static void main(String[] args) {
+
+//		int[] intArray = {10,45,87,65,20,93};
+//		System.out.println(Arrays.toString(intArray));
+		
+		DynamicArray< Integer> dynamicArray = new DynamicArray<>(3);
+		dynamicArray.insert(33);
+		dynamicArray.insert(63);
+		dynamicArray.insert(23);
+		dynamicArray.insert(99);
+		dynamicArray.removeAt(2);
+		dynamicArray.printDynamicArray();
+		System.out.println("Size of the Dynamic Array : "+dynamicArray.dynamicArraySize());
+		System.out.println("The item is found at inex : "+dynamicArray.searchItem(99));
+	}
+
+}
